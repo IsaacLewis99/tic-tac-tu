aeb6c8d62c4c2c5ad781e123c92680490b1df14e
omar khan <omar.khan@cgi.com>
2019-01-09 11:54:04 +0000

    connecting to a database code

---
+++ migrations/02_CREATE_TABLE.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "tic-tac-tu"
+});
+
+var sql_create_users_table = "CREATE TABLE users(user_id SERIAL PRIMARY KEY, name VARCHAR(60), email VARCHAR(60), password TEXT);"
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query(sql_create_users_table, function (err, result) {
+    if (err) throw err;
+    console.log("Table USERS created");
+  });
+});
---
+++ routes/sql.js
@@ -0,0 +1,65 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://root:password@127.0.0.1:3306/acebook');
+
+sequelize.authenticate().then(() => {
+  console.log('Connection established');
+}).catch (err => {
+  console.error('Unable to connect: ', err);
+});
+const Users = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+});
+exports.register = function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  Users.create({
+    name: name,
+    email: email,
+    password: password
+  });
+  res.redirect('/login');
+};
+exports.login = function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  Users.findOne({
+    where: {email: email}
+  }).then(users => {
+    if (users == null) {
+
+      res.send({
+        "code":204,
+        "fail":"Email address not found"
+      });
+    }
+    else if (password == users.password){
+
+      req.session.email = users.email;
+      req.session.name = users.name;
+      req.session.userID = users.id;
+      console.log('User ' + req.session.email + ' logged in, id: ' + req.session.userID);
+      res.redirect('/play');
+    }
+    else {
+      res.send({
+        "code":204,
+        "fail":"Email and password does not match"
+      });
+    }
+  });
+};
